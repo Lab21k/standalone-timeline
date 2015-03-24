aa8159a7603aa0aaef3ec7db9dd6057339176108
André Abadesso <andre.abadesso@gmail.com>
2015-03-24 19:05:30 -0300

    refactor(scripts): removed old jquery, removed from simile-ajax and added cdn

--- scripts/history.js
+++ scripts/history.js
@@ -1,8 +1,8 @@
 /*======================================================================
  *  History
  *
- *  This is a singleton that keeps track of undoable user actions and 
- *  performs undos and redos in response to the browser's Back and 
+ *  This is a singleton that keeps track of undoable user actions and
+ *  performs undos and redos in response to the browser's Back and
  *  Forward buttons.
  *
  *  Call addAction(action) to register an undoable user action. action
@@ -27,14 +27,11 @@ SimileAjax.History = {
     maxHistoryLength:       10,
     historyFile:            "__history__.html",
     enabled:               true,
-    
     _initialized:           false,
     _listeners:             new SimileAjax.ListenerQueue(),
-    
     _actions:               [],
     _baseIndex:             0,
     _currentIndex:          0,
-    
     _plainDocumentTitle:    document.title
 };
 
@@ -46,7 +43,7 @@ SimileAjax.History.initialize = function() {
     if (SimileAjax.History._initialized) {
         return;
     }
-    
+
     if (SimileAjax.History.enabled) {
         var iframe = document.createElement("iframe");
         iframe.id = "simile-ajax-history";
@@ -57,10 +54,10 @@ SimileAjax.History.initialize = function() {
         iframe.style.left = "0px";
         iframe.style.visibility = "hidden";
         iframe.src = SimileAjax.History.historyFile + "?0";
-        
+
         document.body.appendChild(iframe);
         SimileAjax.DOM.registerEvent(iframe, "load", SimileAjax.History._handleIFrameOnLoad);
-        
+
         SimileAjax.History._iframe = iframe;
     }
     SimileAjax.History._initialized = true;
@@ -68,38 +65,36 @@ SimileAjax.History.initialize = function() {
 
 SimileAjax.History.addListener = function(listener) {
     SimileAjax.History.initialize();
-    
     SimileAjax.History._listeners.add(listener);
 };
 
 SimileAjax.History.removeListener = function(listener) {
     SimileAjax.History.initialize();
-    
     SimileAjax.History._listeners.remove(listener);
 };
 
 SimileAjax.History.addAction = function(action) {
     SimileAjax.History.initialize();
-    
     SimileAjax.History._listeners.fire("onBeforePerform", [ action ]);
+
     window.setTimeout(function() {
         try {
             action.perform();
             SimileAjax.History._listeners.fire("onAfterPerform", [ action ]);
-                
             if (SimileAjax.History.enabled) {
                 SimileAjax.History._actions = SimileAjax.History._actions.slice(
                     0, SimileAjax.History._currentIndex - SimileAjax.History._baseIndex);
-                    
+
                 SimileAjax.History._actions.push(action);
                 SimileAjax.History._currentIndex++;
-                
+
                 var diff = SimileAjax.History._actions.length - SimileAjax.History.maxHistoryLength;
+
                 if (diff > 0) {
                     SimileAjax.History._actions = SimileAjax.History._actions.slice(diff);
                     SimileAjax.History._baseIndex += diff;
                 }
-                
+
                 try {
                     SimileAjax.History._iframe.contentWindow.location.search = 
                         "?" + SimileAjax.History._currentIndex;
@@ -134,36 +129,34 @@ SimileAjax.History._handleIFrameOnLoad = function() {
      *  navigates backward or forward. We need to adjust
      *  the application's state accordingly.
      */
-    
     try {
         var q = SimileAjax.History._iframe.contentWindow.location.search;
         var c = (q.length == 0) ? 0 : Math.max(0, parseInt(q.substr(1)));
-        
+
         var finishUp = function() {
             var diff = c - SimileAjax.History._currentIndex;
             SimileAjax.History._currentIndex += diff;
             SimileAjax.History._baseIndex += diff;
-                
             SimileAjax.History._iframe.contentWindow.location.search = "?" + c;
         };
-        
+
         if (c < SimileAjax.History._currentIndex) { // need to undo
             SimileAjax.History._listeners.fire("onBeforeUndoSeveral", []);
             window.setTimeout(function() {
                 while (SimileAjax.History._currentIndex > c && 
                        SimileAjax.History._currentIndex > SimileAjax.History._baseIndex) {
-                       
+
                     SimileAjax.History._currentIndex--;
-                    
+
                     var action = SimileAjax.History._actions[SimileAjax.History._currentIndex - SimileAjax.History._baseIndex];
-                    
+
                     try {
                         action.undo();
                     } catch (e) {
                         SimileAjax.Debug.exception(e, "History: Failed to undo action {" + action.label + "}");
                     }
                 }
-                
+
                 SimileAjax.History._listeners.fire("onAfterUndoSeveral", []);
                 finishUp();
             }, 0);
@@ -172,18 +165,18 @@ SimileAjax.History._handleIFrameOnLoad = function() {
             window.setTimeout(function() {
                 while (SimileAjax.History._currentIndex < c && 
                        SimileAjax.History._currentIndex - SimileAjax.History._baseIndex < SimileAjax.History._actions.length) {
-                       
+
                     var action = SimileAjax.History._actions[SimileAjax.History._currentIndex - SimileAjax.History._baseIndex];
-                    
+
                     try {
                         action.perform();
                     } catch (e) {
                         SimileAjax.Debug.exception(e, "History: Failed to redo action {" + action.label + "}");
                     }
-                    
+
                     SimileAjax.History._currentIndex++;
                 }
-                
+
                 SimileAjax.History._listeners.fire("onAfterRedoSeveral", []);
                 finishUp();
             }, 0);
@@ -192,7 +185,7 @@ SimileAjax.History._handleIFrameOnLoad = function() {
             var title = (index >= 0 && index < SimileAjax.History._actions.length) ?
                 SimileAjax.History.formatHistoryEntryTitle(SimileAjax.History._actions[index].label) :
                 SimileAjax.History._plainDocumentTitle;
-                
+
             SimileAjax.History._iframe.contentWindow.document.title = title;
             document.title = title;
         }
